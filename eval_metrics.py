7d92e7ee0b445c7d3cacb4f36accf83cb94ceb57
giesan <niklas.giesa@charite.de>
2022-11-21 16:38:38 +0000

    ad eval

--- eval_metrics.py
+++ eval_metrics.py
@@ -1,4 +1,24 @@
-from sklearn.metrics import mean_squared_error
-from data_load import load_miss_values
+from pandas.core.frame import DataFrame
+import pandas as pd
 
+def calc_error(df_gt: DataFrame = None, df_imp: DataFrame = None, time_vars: list = [], metric = None, df_miss = None):
+    '''
+    Calculates the error per variable between actual and imputed values
+            Parameters:
+                    df_gt (DataFrame): Ground thruth dataframe with real values
+                    df_imp (DataFrame): Imputed dataframe 
+                    time_vars (list): Column names for imp variables
+                    metric (metric): (Sklearn) Metric to calculate the error
+                    df_miss (DataFrame): df with miss pattenr, if specified just include imputed vales
 
+            Returns:
+                    df (DataFrame): Df with error per variable
+    '''
+    if df_miss is not None:
+        error = [metric(df_gt.iloc[df_miss[df_miss[v].isna()].index][v], 
+                        df_imp.iloc[df_miss[df_miss[v].isna()].index][v]) 
+                        for v in time_vars]
+    else:
+        error = [metric(df_gt[v], df_imp[v]) for v in time_vars]
+    return pd.DataFrame({"variable": time_vars, "error": error})
+    
